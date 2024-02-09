c250027c2eb08a7181cfd52c91b5efef94df1b41
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-02-09 16:47:20 +0000

    fix account profile image error

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
 
 
 class Profile(models.Model):
@@ -11,8 +9,9 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = CloudinaryField('image', default='placeholder')
-    
+    image = models.ImageField(
+        upload_to='images/', default='../placeholder'
+    )
 
     class Meta:
         ordering = ['-created_at']

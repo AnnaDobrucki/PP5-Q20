05296ac410deae46898c33f1c42e09a907437b2a
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-02-26 16:58:15 +0000

    fix rename url dnd_event bug

--- dnd_events/urls.py
+++ dnd_events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DNDEventList, DNDEventDetail
 
 urlpatterns = [
-    path('events/', DNDEventList.as_view(), name='dnd-event-list'),
-    path('events/<int:pk>/', DNDEventDetail.as_view(), name='dnd-event-detail'),
+    path('dnd_events/', DNDEventList.as_view(), name='dnd-event-list'),
+    path('dnd_events/<int:pk>/', DNDEventDetail.as_view(), name='dnd-event-detail'),
 ]

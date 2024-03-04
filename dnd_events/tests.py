abc58238588992b6a0ef2df2453c18558c4dc430
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-03-04 16:29:07 +0000

    add two tests for dnd_events api

--- dnd_events/tests.py
+++ dnd_events/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import DNDEvent
 
-# Create your tests here.
+
+class DNDEventListViewTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='test'
+        )
+        self.other_user = User.objects.create_user(
+            username='otheruser', password='othertest'
+        )
+
+        self.dnd_event_data = {
+            'game_name': 'Test Game',
+            'game_description': 'Test Description',
+            'owner': self.user
+        }
+
+    def test_user_logged_in_can_list_dnd_events(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/dnd_events/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DNDEventModelTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='annauser', password='test'
+        )
+
+    def test_create_dnd_event(self):
+        dnd_event_data = {
+            'game_name': 'DND Test Game',
+            'game_description': 'DND est Description',
+            'owner': self.user,
+        }
+        initial_count = DNDEvent.objects.count()
+
+        dnd_event = DNDEvent.objects.create(**dnd_event_data)
+
+        self.assertEqual(DNDEvent.objects.count(), initial_count + 1)
+        self.assertEqual(dnd_event.game_name, 'Test Game')
+
+

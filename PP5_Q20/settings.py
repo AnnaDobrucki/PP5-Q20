831137507aa1a0d09ca2e2f8bf7ba32de3b36ce1
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-03-11 16:22:13 +0000

    re fix cors otigin issue setting.py

--- PP5_Q20/settings.py
+++ PP5_Q20/settings.py
@@ -123,6 +123,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
 CORS_ALLOWED_ORIGIN_REGEXES = [ 
     r"^https://.*\.gitpod\.io$",
 ]
